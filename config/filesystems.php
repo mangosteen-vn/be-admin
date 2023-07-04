3d5a3eb612f542dfa43969b0a67b4c26e3352ad5
thienbd <thienbd@vmms.com.vn>
2023-07-04 22:01:16 +0700

    wip crud product with gallery & collection

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,10 +38,15 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
-            'throw' => false,
+        ],
+
+        'media' => [
+            'driver' => 'local',
+            'root'   => public_path('uploads'),
+            'url'    => env('APP_URL').'/uploads',
         ],
 
         's3' => [
